deeae6af3444df86a06562ea1f88306f78d68dfe
luckysetiawan <luckysetiawan0802@gmail.com>
2022-12-11 14:38:34 +0700

    added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayCity()
+    {
+        SceneManager.LoadScene("City");
+    }
+    public void PlayLab()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
